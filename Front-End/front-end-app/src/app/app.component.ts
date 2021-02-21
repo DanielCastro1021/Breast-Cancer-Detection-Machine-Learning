16d22bbb1c78c3bdec3424cff830f176884d909b
Daniel Castro <danielcastrofafe@gmail.com>
2021-02-21 12:46:56 +0000

    Updated Home Page Component.

--- Front-End/front-end-app/src/app/app.component.ts
+++ Front-End/front-end-app/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AlertDialogComponent } from './alert-dialog/alert-dialog.component';
+import { Diagnose } from './models/Diagnose';
+import { MachineLearningApiService } from './services/machine-learning-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'front-end-app';
+  @Input() diagnose: Diagnose = {};
+
+  constructor(
+    private api: MachineLearningApiService,
+    private dialog: MatDialog
+  ) {}
+
+  public getDiagnose(): void {
+    console.log(this.diagnose);
+    this.api.getDiagnose(this.diagnose).subscribe((diagnose_result) => {
+      console.log(diagnose_result);
+      this.showDiagnoseResult(diagnose_result);
+    });
+  }
+
+  private showDiagnoseResult(diagnose_result: number): void {
+    let dialogRef = this.dialog.open(AlertDialogComponent, {
+      data: { dataKey: diagnose_result },
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result == 'confirm') {
+        console.log('Diagnose Confirmed.');
+      }
+    });
+  }
 }
--- Front-End/front-end-app/src/app/app.module.ts
+++ Front-End/front-end-app/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { AlertDialogComponent } from './alert-dialog/alert-dialog.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, AlertDialogComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    MatDialogModule,
+    MatButtonModule,
   ],
+  exports: [MatButtonModule, MatDialogModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
